2a419a9341bfebb7b72987acbc86f1605b2ba60c
carolc24 <spellingbee10@gmail.com>
2014-10-20 13:39:28 -0700

    Different FitnessCalculators and subpop ids

--- test/org/fhcrc/honeycomb/metapop/MutationTest.java
+++ test/org/fhcrc/honeycomb/metapop/MutationTest.java
@@ -6,10 +6,11 @@ import org.fhcrc.honeycomb.metapop.Subpopulation;
 import org.fhcrc.honeycomb.metapop.coordinate.Coordinate;
 import org.fhcrc.honeycomb.metapop.RandomNumberUser;
 import org.fhcrc.honeycomb.metapop.fitness.FitnessCalculator;
-import org.fhcrc.honeycomb.metapop.fitness.IdentityCalculator;
+import org.fhcrc.honeycomb.metapop.fitness.MonodCalculator;
 
 import org.fhcrc.honeycomb.metapop.mutation.MutationRule;
 import org.fhcrc.honeycomb.metapop.mutation.MutateCoopCheat;
+import org.fhcrc.honeycomb.metapop.mutation.MutateEachGrowth;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -30,16 +31,19 @@ public class MutationTest {
     private double conf = 0.95;
 
     private RandomNumberUser rng = new RandomNumberUser(1);
-    private FitnessCalculator fc = new IdentityCalculator();
+    private FitnessCalculator fc_anc_coop = new MonodCalculator(0.45, 10.0, 0.1, 200);
+    private FitnessCalculator fc_anc_cheat = new MonodCalculator(0.54, 10.0, 0.083, 200);
+    private FitnessCalculator fc_evo_coop = new MonodCalculator(0.315, 1.0, 0.05, 200);
+    private FitnessCalculator fc_evo_cheat = new MonodCalculator(0.378, 1.0, 0.042, 200);
     private Coordinate coord = new Coordinate(1,1);
 
     private double resource = 0.0;
     private int large_subpop_size = 10000;
     private int small_subpop_size = 300;
-    private String anc_coop_id = "anc_coop";
-    private String anc_cheat_id = "anc_cheat";
-    private String evo_coop_id = "evo_coop";
-    private String evo_cheat_id = "evo_cheat";
+    private String anc_coop_id = "coop_0.450_10.0";
+    private String anc_cheat_id = "cheat_0.540_10.0";
+    private String evo_coop_id = "coop_0.315_1.0";
+    private String evo_cheat_id = "cheat_0.378_1.0";
     private Subpopulation anc_coop;
     private Subpopulation anc_cheat; 
     private Subpopulation evo_coop;
@@ -54,12 +58,12 @@ public class MutationTest {
 
     @Before
     public void setUp() {
-        anc_coop = new Subpopulation(large_subpop_size, fc, anc_coop_id, rng);
+        anc_coop = new Subpopulation(large_subpop_size, fc_anc_coop, anc_coop_id, rng);
         anc_cheat = 
-            new Subpopulation(large_subpop_size, fc, anc_cheat_id, rng);
-        evo_coop = new Subpopulation(small_subpop_size, fc, evo_coop_id, rng);
+            new Subpopulation(large_subpop_size, fc_anc_cheat, anc_cheat_id, rng);
+        evo_coop = new Subpopulation(small_subpop_size, fc_evo_coop, evo_coop_id, rng);
         evo_cheat = 
-            new Subpopulation(small_subpop_size, fc, evo_cheat_id, rng);
+            new Subpopulation(small_subpop_size, fc_evo_cheat, evo_cheat_id, rng);
         subpops = 
             new ArrayList<Subpopulation>(Arrays.asList(anc_coop, anc_cheat,
                                                        evo_coop, evo_cheat));
@@ -84,21 +88,39 @@ public class MutationTest {
             List<Population> pops =
                 new ArrayList<Population>(Arrays.asList(pop));
             Population pop_copy = pops.get(0);
+            List<Population> pops_new = 
+                new ArrayList<Population>(Arrays.asList(pop));
+            Population pop_copy_new = pops_new.get(0);
 
             MutationRule mutate_coop_cheat =
                 new MutateCoopCheat(coop_to_cheat, 0, rng);
 
             mutate_coop_cheat.mutate(pops);
 
-            int coop_after = pop_copy.getSubpopById("anc_coop").getSize();
-            int cheat_after = pop_copy.getSubpopById("anc_cheat").getSize();
+            int coop_after = pop_copy.getSubpopById("coop_0.450_10.0").getSize();
+            int cheat_after = pop_copy.getSubpopById("cheat_0.540_10.0").getSize();
             int coop_change = Math.abs(coop_before-coop_after);
             int cheat_change = Math.abs(cheat_before-cheat_after);
+
+            MutationRule mutate_each_growth = 
+                new MutateEachGrowth(0, coop_to_cheat, 0, rng);
+
+            mutate_each_growth.mutate(pops_new);
+
+            int coop_after_new = pop_copy_new.getSubpopById("coop_0.450_10.0").getSize();
+            int cheat_after_new = pop_copy_new.getSubpopById("cheat_0.540_10.0").getSize();
+            int coop_change_new = Math.abs(coop_before-coop_after_new);
+            int cheat_change_new = Math.abs(cheat_before-cheat_after_new);
+
+            int coop_change_difference = Math.abs(coop_change - coop_change_new);
+            int cheat_change_difference = Math.abs(cheat_change - cheat_change_new);
 
             assertEquals("exchange wrong", coop_change, cheat_change, 0.0);
 
             double[] ci = expected.getCI();
-            if (coop_change < ci[0] || coop_change > ci[1]) failed++;
+            if (coop_change < ci[0] || coop_change > ci[1] || 
+                coop_change_new < ci[0] || coop_change_new > ci[1]) failed++;
+            if (coop_change_difference != 0) System.out.println("Warning: discrepancy of " + coop_change_difference);
         }
         FailReport.report("[PopulationTest] mutateCoopCheat()", failed,
                           mutation_rates.length, expect_fail);
@@ -121,21 +143,39 @@ public class MutationTest {
             List<Population> pops =
                 new ArrayList<Population>(Arrays.asList(pop));
             Population pop_copy = pops.get(0);
+            List<Population> pops_new = 
+                new ArrayList<Population>(Arrays.asList(pop));
+            Population pop_copy_new = pops_new.get(0);
 
             MutationRule mutate_coop_cheat =
                 new MutateCoopCheat(0, cheat_to_coop, rng);
 
             mutate_coop_cheat.mutate(pops);
 
-            int coop_after = pop_copy.getSubpopById("anc_coop").getSize();
-            int cheat_after = pop_copy.getSubpopById("anc_cheat").getSize();
+            int coop_after = pop_copy.getSubpopById("coop_0.450_10.0").getSize();
+            int cheat_after = pop_copy.getSubpopById("cheat_0.540_10.0").getSize();
             int coop_change = Math.abs(coop_before-coop_after);
             int cheat_change = Math.abs(cheat_before-cheat_after);
+
+            MutationRule mutate_cheats = 
+                new MutateEachGrowth(0, 0, cheat_to_coop, rng);
+
+            mutate_cheats.mutate(pops_new);
+
+            int coop_after_new = pop_copy_new.getSubpopById("coop_0.450_10.0").getSize();
+            int cheat_after_new = pop_copy_new.getSubpopById("cheat_0.540_10.0").getSize();
+            int coop_change_new = Math.abs(coop_before-coop_after_new);
+            int cheat_change_new = Math.abs(cheat_before-cheat_after_new);
+
+            int coop_change_difference = Math.abs(coop_change - coop_change_new);
+            int cheat_change_difference = Math.abs(cheat_change - cheat_change_new);
 
             assertEquals("exchange wrong", coop_change, cheat_change, 0.0);
 
             double[] ci = expected.getCI();
-            if (cheat_change < ci[0] || cheat_change > ci[1]) failed++;
+            if (cheat_change < ci[0] || cheat_change > ci[1] || 
+                cheat_change_new < ci[0] || cheat_change_new > ci[1]) failed++;
+            if (cheat_change_difference != 0) System.out.println("Warning: discrepancy of " + coop_change_difference);
         }
         FailReport.report("[PopulationTest] mutateCheatCoop()", failed,
                           mutation_rates.length, expect_fail);
